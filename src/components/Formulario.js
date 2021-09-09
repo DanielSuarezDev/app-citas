bec5ed47fcac250d33d6177ff78f16036be5909d
Daniel Suarez <danielsuarez3114@gmail.com>
2021-09-09 14:58:52 -0500

    form

---
+++ src/components/Formulario.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+export const Formulario = () => {
+  const [cita, setCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  const [error, setError] = useState(false)
+  const handleChange = (e) => {
+    setCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // validar
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      setError(true)
+      return;
+    }
+
+   
+    // asignar ID
+
+    // Crear cita
+
+    // Reiniciar Form
+  };
+  return (
+    <div>
+      <h2>Crear Cita</h2>
+
+        {error ? <p className='alerta-error'>Todos los campos son obligatorios</p>: null}
+
+      <form onSubmit={handleSubmit}>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={handleChange}
+          value={mascota}
+        />
+
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño mascota"
+          onChange={handleChange}
+          value={propietario}
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={handleChange}
+          value={fecha}
+        />
+
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+          value={hora}
+        />
+
+        <label>Síntomas</label>
+        <textarea
+          className="u-full-width"
+          onChange={handleChange}
+          name="sintomas"
+          value={sintomas}
+        ></textarea>
+
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </div>
+  );
+};
